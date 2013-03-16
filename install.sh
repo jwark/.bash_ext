7311e35ddf5adbdf2e45445e319c5103fb6dc8e4
Justin Wark <justinwark@gmail.com>
2013-03-16 14:57:08 +1100

    improve install script.

--- install.sh
+++ install.sh
@@ -2,17 +2,26 @@ BASH_EXT="$HOME/.jw_bash_ext"
 echo "Installing bash extensions for '$USER' to '$BASH_EXT'..."
 
 touch $BASH_EXT
-curl -L "https://raw.github.com/jwark/.bash_ext/master/.bash_ext" -o $BASH_EXT
-source $BASH_EXT
+curl -L# "https://raw.github.com/jwark/.bash_ext/master/.bash_ext" -o $BASH_EXT
+SOURCE_CMD="source $BASH_EXT"
+$SOURCE_CMD
+
+function addSourceIfNeeded {
+  DOT_FILE=$1
+
+  if ! grep -Fxq "$SOURCE_CMD" $DOT_FILE
+  then
+      echo "adding to $DOT_FILE"
+      echo $SOURCE_CMD >> $DOT_FILE
+  fi
+}
 
 if [ -f "$HOME/.bash_profile" ]
 then
-  echo "source $BASH_EXT" >> $HOME/.bash_profile
+  addSourceIfNeeded "$HOME/.bash_profile"
+elif [ -f "$HOME/.bashrc" ]
+then
+  addSourceIfNeeded "$HOME/.bashrc"
 else
-  if [ -f "$HOME/.bashrc" ]
-  then
-    echo "source $BASH_EXT" >> $HOME/.bashrc
-  else
-    echo "Cannot find .bashrc or .bash_profile, extensions not installed."
-  fi
+  echo "Cannot find .bashrc or .bash_profile, extensions not installed."
 fi
